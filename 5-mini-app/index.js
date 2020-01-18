7d5b1a2661c0f2fd3a43d60ed7e56720da0c3d57
eilisglee <elee2@seattleu.edu>
2020-01-18 11:59:18 -0800

    finished 5-mini-app

--- 5-mini-app/index.js
+++ 5-mini-app/index.js
@@ -10,3 +10,16 @@
 // get an integer using getargs
 let getargs = require("../modules/getargs/index.js");
 let input = getargs.getIntegerArg();
+
+
+if (isNaN(input) || !Number.isInteger(input)) {
+    console.log("usage: usd to euro <integer>");
+  } else {
+    let USD = Math.floor((input / 1));
+    let EURO = input * 0.9;
+  
+    let usd = (USD == 1) ? "US dollar" : "US dollars";
+    let euro = (EURO == 0.9) ? "Euro" : "Euros";
+    
+    console.log(`${input} USD is ${USD} ${usd} or ${EURO} ${euro} `)
+  }
